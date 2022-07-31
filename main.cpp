d61c7d995e01e45dd926665bf28127e72fd179f7
Anthony Rubio <Swiftcaster.dev@gmail.com>
2022-07-31 13:51:35 -0400

    leassionstart

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <cstdlib>
+#include <iostream> // cout, endl
+#include <string>
+#include <limits>
+#include <vector>
+#include <sstream>
+#include <numeric>
+#include <ctime>
+#include <cmath>
+
+#include <algorithm> // sort
+// This is a comment!
+
+using namespace std;
+
+int imGlobal = 0;
+const double PI = 3.14;
+
+int main(int argc, char** argv) {
+
+    std::cout << "Hello, World!" << std::endl;
+
+    bool married = true;
+    char myGrade = 'A';
+
+    return 0;
+}
